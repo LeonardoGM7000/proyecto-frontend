0805bdc7ccc1470af53431f28b602553cc049820
EmiJuas <emidh4434@gmail.com>
2023-12-16 00:40:37 -0600

    Ajustes de tablas y filtros para info específica

--- js/pruebaTabla.js
+++ js/pruebaTabla.js
@@ -1,62 +1,49 @@
+let datosClima = JSON.parse(jsonClima);
+let arrClimas = Array.from(datosClima);
+let filasClima = "";
+let ides = new Array();
+let estados = new Array();
+let dias = new Array();
+
+arrClimas.forEach((obj) =>{
+  let dloc = new String(obj.dloc);
+  let dia = "".concat(dloc.valueOf().slice(6,8),"/",dloc.valueOf().slice(4,6),"/",dloc.valueOf().slice(0,4));
+
+  filasClima += `
+    <tr>
+      <td>${obj.nes}</td>
+      <td>${obj.nmun}</td>
+      <td>${dia}</td>    
+      <td>${obj.tmax}</td>
+      <td>${obj.tmin}</td>
+      <td>${obj.desciel}</td>
+      <td>${obj.probprec}</td>
+    </tr>
+  `;
+  let nombreEs = new String(obj.nes);
+  if(!estados.includes(nombreEs.valueOf())){
+    estados.push(nombreEs.valueOf());
+  }
+  if(!dias.includes(dia)){
+    dias.push(dia);
+  }
+});
+console.log(dias);
+let opcEstados = "";
+opcEstados += `
+  <option value="0" selected>Seleccione un estado</option>
+`;
+estados.forEach((obj) =>{
+  opcEstados += `
+    <option value="${obj}">${obj}</option>
+  `;
+});
+
 $(document).ready(() => {
-  let datosClima = JSON.parse(jsonClima);
-  let arrClimas = Array.from(datosClima);
-  let filasClima = "";
-
-  arrClimas.forEach((obj) =>{
-    let dia = "";
-    if(obj.dloc=="20231109T00"){
-      dia = "09/11/2023";
-    } else if(obj.dloc=="20231110T00"){
-      dia = "10/11/2023";
-    } else if(obj.dloc=="20231111T00"){
-      dia = "11/11/2023";
-    } else{
-      dia = "12/11/2023";
-    }
-
-    filasClima += `
-      <tr>
-        <td>${obj.nes}</td>
-        <td>${obj.nmun}</td>
-        <td>${dia}</td>    
-        <td>${obj.tmax}</td>
-        <td>${obj.tmin}</td>
-        <td>${obj.desciel}</td>
-        <td>${obj.probprec}</td>
-      </tr>
-    `;
-  });
-
+  $("#filas").html(filasClima);
   $("#filasClima").html(filasClima);
-
-  /*
-  $("#diaSel").change(()=>{
-    let diaSel = $("#diaSel").val();
-    
-
-    if(diaSel=="0"){
-      
-    }else if(diaSel=="1"){
-      arrClimas.forEach((obj) =>{
-        if(obj.dloc=="20231109T00"){
-          filasClima += `
-            <tr>
-              <td>${obj.nes}</td>
-              <td>${obj.nmun}</td>
-              <td>${obj.tmax}</td>
-              <td>${obj.tmin}</td>
-              <td>${obj.desciel}</td>
-              <td>${obj.probprec}</td>
-            </tr>
-          `;
-        }
-      });
-    }
-  });*/
-  
-  $('#myTable').DataTable({
-    "order": [[ 0, 'asc' ], [ 1, 'asc' ], [ 2, 'asc' ]],
+  $('#tablaClima').DataTable({
+    "order": [[ 0, 'asc' ]],
     language: {
       info: 'Mostrando página _PAGE_ de _PAGES_',
       infoEmpty: 'No hay registros disponibles',
@@ -73,45 +60,62 @@ $(document).ready(() => {
     },
     pagingType: "full_numbers",
     "lengthMenu": [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "All"] ],
-    /*responsive: true,
-    columnDefs: [
-        { responsivePriority: 1, targets: 0 },
-        { responsivePriority: 2, targets: -1 }
-    ]*/
-    responsive: {
-      details: false
+    "info": false,
+  });
+
+  $("#estadoSel").html(opcEstados);
+
+  $("#estadoSel").change(() => {
+    $("div#municipioSel").addClass("visually-hidden");
+    $("div#diaSel").addClass("visually-hidden");
+    let estado = $("#estadoSel").val();
+    if(estado != "0"){
+      let opcMunicipios = "";
+      let municipios = new Array();
+      arrClimas.forEach((obj) =>{
+        if(obj.nes == estado && obj.ndia == "0"){
+          let nombreMun = new String(obj.nmun);
+          if(!municipios.includes(nombreMun.valueOf())){
+            municipios.push(nombreMun.valueOf());
+          }
+        }
+      });
+      opcMunicipios += `
+        <option value="0" selected>Seleccione un municipio</option>
+      `;
+      municipios.forEach((obj) =>{
+        opcMunicipios += `
+          <option value="${obj}">${obj}</option>
+        `;
+      });
+      $("select#municipioSel").html(opcMunicipios);
+      $("div#municipioSel").removeClass("visually-hidden");
+
+
+    } else{
+      $("div#municipioSel").addClass("visually-hidden");
+      $("div#diaSel").addClass("visually-hidden");
     }
-    /*initComplete: function () {
-      this.api()
-          .columns()
-          .every(function () {
-              let column = this;
+  });
 
-              // Create select element
-              let select = document.createElement('select');
-              select.add(new Option(''));
-              column.footer().replaceChildren(select);
+  $("select#municipioSel").change(() => {
+    $("div#diaSel").addClass("visually-hidden");
+    let municipio = $("select#municipioSel").val();
+    if(municipio != "0"){
+      let opcDias = "";
+      opcDias += `
+        <option value="0" selected>Seleccione un día</option>
+      `;
+      dias.forEach((obj) =>{
+        opcDias += `
+          <option value="${obj}">${obj}</option>
+        `;
+      });
+      $("select#diaSel").html(opcDias);
+      $("div#diaSel").removeClass("visually-hidden");
 
-              // Apply listener for user change in value
-              select.addEventListener('change', function () {
-                  var val = DataTable.util.escapeRegex(select.value);
-
-                  column
-                      .search(val ? '^' + val + '$' : '', true, false)
-                      .draw();
-              });
-
-              // Add list of options
-              column
-                  .data()
-                  .unique()
-                  .sort()
-                  .each(function (d, j) {
-                      select.add(new Option(d));
-              select.append('<option value="' + d + '">' + d.substr(0,30) + '</option>')
-
-                  });
-          });
-      },*/
+    } else{
+      $("div#diaSel").addClass("visually-hidden");
+    }
   });
 });
